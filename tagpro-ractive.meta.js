d90dca2209c82804aaab054ee2f73353b963a814
Kera <keratagpro@gmail.com>
2015-01-21 18:00:42 +0200

    Updates

---
+++ tagpro-ractive.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          TagPro Ractive
+// @description   A base object that listens to the global tagpro object using Ractive's magic mode.
+// @version       0.1
+// @author        Kera
+// @namespace     http://keratagpro.github.io/tagpro-ractive
+// @downloadUrl   https://keratagpro.github.io/tagpro-ractive/tagpro-ractive.user.js
+// @updateUrl     https://keratagpro.github.io/tagpro-ractive/tagpro-ractive.meta.js
+// @include       http://tagpro-*.koalabeast.com:*
+// @include       http://tangent.jukejuice.com:*
+// @include       http://*.newcompte.fr:*
+// @require       http://cdn.ractivejs.org/latest/ractive.min.js
+// ==/UserScript==
